4bbf80f26cfa325d251037f0e509e61aa9777629
jhonny <jhonny.anovo@gmail.com>
2020-11-29 20:09:52 -0500

    primeros cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { HeroesComponent } from './component/heroes/heroes.component';
+import { RouterModule,Routes } from '@angular/router';
+
+
+const appRoutes:Routes =[
+  {path:'**',pathMatch:'full',redirectTo:'home'},
+ 
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'heroes',component:HeroesComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
